2b7efe5581862f3b1901e40fb4e41e63c7acba74
Pedro Miguel <pedromiguellitoo@gmail.com>
2025-02-13 12:41:46 -0300

    fix: fixing reference to curriculum vitae

--- src/app/components/Navigation/Navigation.jsx
+++ src/app/components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import { FiX } from "react-icons/fi";
 import style from "./Navigation.module.css";
-import CurriculumVitae from "../../../assets/curriculum-vitae/CV - Pedro Miguel de Paula Silva.docx"
+import CurriculumVitae from "../../../assets/curriculum-vitae/Curriculum Vitae - Pedro Miguel.docx"
 
 const navItems = [
   { name: "Home", link: "#home" },
